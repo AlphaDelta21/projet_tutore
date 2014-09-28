0c0de6d11d05cb4a11ddb80098acea2a956c4442
Doudie21 <elodie.dumasmarliens@gmail.com>
2014-09-28 17:48:18 +0200

    interface du prof

--- interfaces/interface1.php
+++ interfaces/interface1.php
@@ -8,10 +8,10 @@
 		<link rel="shortcut icon" type="image/x-icon" href="../images/iut-dijon.jpg" />
 		<meta name="generator" content="Bluefish 2.2.5" >
 		<meta name="author" content="elodie" >
-		<meta name="date" content="2014-09-28T14:54:40+0200" >
-		<meta name="copyright" content="www.asq-gr.fr">
-		<meta name="keywords" content="gr, grs, asq, quetigny, site, côte d'or, bourgogne, gymnastique, club, association, compétition">
-		<meta name="description" content="Site du club de GRS a Quetigny">
+		<meta name="date" content="2014-09-28T17:44:09+0200" >
+		<meta name="copyright" content="www.nomsite.fr">
+		<meta name="keywords" content="">
+		<meta name="description" content="Site de sondages de l'IUT informatique de Dijon">
 		<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta http-equiv="content-style-type" content="text/css">
@@ -19,13 +19,36 @@
 	</head>
 
 <body>
-	<div>
-		coucou
+
+<div>
+	<div class="gauche">
 	</div>
 	
-	<div>
+	<div class="contenuprincipal">
+		
+		<div class="descriptionprof">
+			Nom professeur, matière, moubfbc
+		</div>
+		
+		<div class="historique">
+			Historique des questions déja posées		
+		</div>
+		
+		<div class="newquestion">
+			Nouvelle question		
+		</div>
+		
 	</div>
 	
+	<div class="droite">
+	</div>
+	
+	<div class="piedpage">
+		développé par XXX
+	</div>
+</div>
+
+
 </body>
 
 </html>
