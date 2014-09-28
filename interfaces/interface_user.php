12baa3a0c3e33d547f264a82d82f15582d3f2e19
alex viv <alexis.viverge@gmail.com>
2014-09-28 17:26:30 +0200

    fond
    notre fond commun

---
+++ interfaces/interface_user.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang='fr'>
+
+	<head>
+		<title>IUT Dijon informatique</title>
+		<meta charset="utf-8">
+		<link rel="shortcut icon" type="image/x-icon" href="../images/iut-dijon.jpg" />
+		
+        <meta name="generator" content="Bluefish 2.2.5" >
+		<meta name="author" content="elodie" >
+		<meta name="date" content="2014-09-28T14:54:40+0200" >
+		<meta name="copyright" content="www.asq-gr.fr">
+		<meta name="keywords" content="gr, grs, asq, quetigny, site, côte d'or, bourgogne, gymnastique, club, association, compétition">
+		<meta name="description" content="Site du club de GRS a Quetigny">
+		<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta http-equiv="content-style-type" content="text/css">
+		<meta http-equiv="expires" content="0">
+	</head>
+
+<body>
+	
+    <div class="div_centre">
+    
+   
+	</div>
+    <?php include 'includes/footer.php' ;?>
+	
+</body>
+
+</html>
