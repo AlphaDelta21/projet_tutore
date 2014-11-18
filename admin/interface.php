2b29ca22b333bb38501a314a50390c096961f148
Doudie21 <elodie.dumasmarliens@gmail.com>
2014-11-18 22:02:43 +0100

    images
    Ajout de deux images qui permettront de gérer les différentes questions ( modifier et supprimer ).

--- admin/interface.php
+++ admin/interface.php
@@ -6,9 +6,9 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="index.css">
 		<link rel="shortcut icon" type="image/x-icon" href="../images/iut-dijon.jpg" />
-		<meta name="generator" content="Bluefish 2.2.5" >
+		<meta name="generator" content="Bluefish 2.2.6" >
 		<meta name="author" content="elodie" >
-		<meta name="date" content="2014-11-05T18:28:38+0100" >
+		<meta name="date" content="2014-11-18T22:00:29+0100" >
 		<meta name="copyright" content="www.nomsite.fr">
 		<meta name="keywords" content="">
 		<meta name="description" content="Site de sondages de l'IUT informatique de Dijon">
@@ -39,14 +39,28 @@
 					
 					<table style="background-color:#F3F3F3; margin-left: 20px;"> 
 						<tr>
-							<td>test </td>
+							<td>Question</td><td><img src="modifier.png" width="15" height="15" alt=""></td><td><img src="delete.png" width="15" height="15" alt=""></td>
 						</tr>
 					</table>
 					
 				</div>
 				
 				<div class="newquestion">
-					Publier une nouvelle question.		
+					Publier une nouvelle question
+					
+					<form>
+						<label>Question :</label>
+						<textarea></textarea> <br>
+						<label>Réponse 1 :</label>
+						<input type="text" id="rep1" name="rep1"> <br>
+						<label>Réponse 2 :</label>
+						<input type="text" id="rep2" name="rep2"> <br>
+						<label>Réponse 3 :</label>
+						<input type="text" id="rep3" name="rep3"> <br>
+						<label>Réponse 4 :</label>
+						<input type="text" id="rep4" name="rep4"> <br>
+						<input type="submit" id="poster" name="poster" value="Poster">
+					</form>		
 				</div>
 			</div>
 		</div>
