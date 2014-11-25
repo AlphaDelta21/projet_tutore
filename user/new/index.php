449fb224b0477ec88628d2261586c88b31d9f456
alex viv <alexis.viverge@gmail.com>
2014-11-25 18:05:51 +0100

    Captcha OK
    ajout du captcha dans index user

--- user/new/index.php
+++ user/new/index.php
@@ -8,17 +8,16 @@
 <body>
 
 
+
 <div class="div_img">
 	<img src="images/iut-dijon.png" class="img_iut"  alt=""/>
 </div>
-<?php include "captcha.php" ;?>
-
 
 
 <form class="form_page1" action="question.php" method="post">
 	<p class="p_code"> Entrez le code donné par votre professeur :  <input name="txt_code" type="text" required></p>	
     <br>
-    <label for="captcha">Recopiez ce captcha : <?php echo captcha(5); ?></label>
+    <label for="captcha">Recopiez ce captcha : <img src="captcha.php" alt="Captcha" /></label>
 	<input type="text" name="captcha" id="captcha" />
     <br><br>
     <input type="submit" value="Continuer" />
